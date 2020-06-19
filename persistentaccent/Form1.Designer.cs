ec39c787c2c204e558042f97e1f0344ec85ab133
logonoff <logonoff@users.noreply.github.com>
2020-06-19 00:32:16 -0400

    init commit innit

---
+++ persistentaccent/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace persistent_accent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nicoHide = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.end = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nicoHide
+            // 
+            this.nicoHide.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.nicoHide.Text = "Click to view";
+            this.nicoHide.Visible = true;
+            this.nicoHide.MouseClick += new System.Windows.Forms.MouseEventHandler(this.nicoHide_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // end
+            // 
+            this.end.Location = new System.Drawing.Point(99, 119);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(75, 23);
+            this.end.TabIndex = 0;
+            this.end.Text = "exit";
+            this.end.UseVisualStyleBackColor = true;
+            this.end.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.end);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon nicoHide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button end;
+    }
+}
+
---
+++ persistentaccent/Form1.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace persistent_accent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the SystemEvents class to receive event notification when a user 
+            // preference changes, the palette changes, or when display settings change.
+            SystemEvents.UserPreferenceChanged += x;
+
+            // For demonstration purposes, this application sits idle waiting for events.
+            Console.WriteLine("This application is waiting for system events.");
+            Console.WriteLine("Press <Enter> to terminate this application.");
+            Console.ReadLine();
+        }
+
+        readonly static UserPreferenceChangedEventHandler x = new UserPreferenceChangedEventHandler(Thingy);
+
+        /* 
+         * called when a user preference changes
+         */
+        static void Thingy(object sender, UserPreferenceChangedEventArgs e)
+        {
+            Console.WriteLine("The user preference is changing. Category={0}", e.Category);
+
+            System.Threading.Thread.Sleep(1000);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\DWM", true);
+
+            key.SetValue("AccentColor", unchecked((int)0xff000000), RegistryValueKind.DWord);
+
+            key.Close();
+        }
+
+        /*
+         * Hide program in tray
+         * http://smartsnipp.ecomparefiles.com/code/c_sharp_windows_application/windows_form_hide_and_show_in_system_tray.aspx
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            nicoHide.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            nicoHide.BalloonTipTitle = "Hide Form";
+            nicoHide.BalloonTipText = "Form Minimized to System Tray.";
+
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                nicoHide.Visible = true;
+                nicoHide.ShowBalloonTip(4);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                nicoHide.Visible = false;
+            }
+        }
+
+        private void nicoHide_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        /*
+         * i think this is what you do
+         * when you want to unhook a thingy
+         */
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            SystemEvents.UserPreferenceChanged -= x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
